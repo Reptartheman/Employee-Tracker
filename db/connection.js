a191240a093c8895dd5109509dab0378ce22b3d9
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-03-19 19:16:12 -0400

    added connection to sql

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection(
+    {
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        user: process.env.DB_USER,
+      },
+      console.log("\x1b[33m", `Connected to ${process.env.DB_NAME}.`)
+    );
+
+    connection.connect(function (err){
+        if (err) throw err;
+    });
+
+    module.exports = connection;
